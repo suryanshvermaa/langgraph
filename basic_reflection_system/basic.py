c8dc0b472e01665b5ee681dc7166e7321d765ebd
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-22 21:21:14 +0530

    completed

---
+++ basic_reflection_system/basic.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END,MessageGraph
+from chains import generation_chain,reflection_chain
+
+load_dotenv()
+
+graph=MessageGraph()
+
+REFLECT="reflect"
+GENERATE="generate"
+
+def generate_node(state):
+    return generation_chain.invoke({
+        "messages":state
+    })
+
+def reflect_node(state):
+    response=reflection_chain.invoke({
+        "messages":state
+    })
+    return [HumanMessage(content=response.content)]
+
+graph.add_node(GENERATE,generate_node)
+graph.add_node(REFLECT,reflect_node)
+graph.set_entry_point(GENERATE)
+
+def should_continue(state):
+    if (len(state) > 2):
+        return END
+    return REFLECT
+
+graph.add_conditional_edges(GENERATE,should_continue)
+graph.add_edge(REFLECT,GENERATE)
+
+app=graph.compile()
+# print(app.get_graph().draw_mermaid())
+# app.get_graph().print_ascii()
+
+res=app.invoke(HumanMessage(content="Ai agents taking over content creation"))
+
+print(res)
